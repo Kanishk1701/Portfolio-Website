a6f3e000fc7bcf838ea92a626b389e87c18322b7
Kanishk <kanishkpandey1701@gmail.com>
2025-05-27 22:10:01 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base : "/Portfolio-Website", // Set the base path for your project
 })
