b7152b3552560f33daab57d1f0b1ae7e5ce26afd
Kanishk <kanishkpandey1701@gmail.com>
2025-05-27 20:16:52 +0530

    More changes

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -15,13 +15,38 @@ export const Contact = () => {
                 <form className="space-y-6">
                     <div className="relative">  
                         <input type="text" 
-                        id="name" 
-                        name="name" 
+                        id="Name" 
+                        name="Name" 
                         required 
                         className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" 
                         placeholder="Name..."
                         />
                     </div>
+                    <div className="relative">  
+                        <input type="text" 
+                        id="Email" 
+                        name="Email" 
+                        required 
+                        className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" 
+                        placeholder="example@gmail.com"
+                        />
+                    </div>
+                    <div className="relative">  
+                        <textarea
+                        id="message" 
+                        name="message" 
+                        required 
+                        rows={5}
+                        className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5" 
+                        placeholder="Your Message..."
+                        />
+                    </div>
+
+                    <button type="submit"  
+                    className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
+                    Send Message..</button>
+
+
                 </form>
                 </div>
             </RevealOnScroll>
