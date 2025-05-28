0dfa19e65ad3c64a9765516ecf61419eb4c5e965
Kanishk <kanishkpandey1701@gmail.com>
2025-05-28 12:31:15 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
+const isGitHub = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base : "/Portfolio-Website", // Set the base path for your project
+  base: isGitHub ? '/Portfolio-Website/' : '/',  // ✅ Auto-switch base path
 })
