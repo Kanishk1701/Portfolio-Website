f3947ee55c09c88c0560a1f01b41d8c8892ab549
Kanishk <kanishkpandey1701@gmail.com>
2025-05-28 15:06:54 +0530

    fix: vercel config fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const isGitHub = process.env.GITHUB_PAGES === 'true';
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: isGitHub ? '/Portfolio-Website/' : '/',  // ✅ Auto-switch base path
+  base: '/', // Use root for Vercel
 })
